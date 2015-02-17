e2427aea2efaaa207e498adafc975037f96f87d9
Sage Berg <sberg@uoregon.edu>
2015-02-16 19:24:07 -0800

    added printMap function, and wrote rought draft of terrain generation algorithm

--- terrain_generator.cpp
+++ terrain_generator.cpp
@@ -4,7 +4,7 @@
 
 #include <random>
 #include <iostream>
-#include <algorithm>
+#include <algorithm>  // to fill arrays with characters
 
 #include "generator.hpp"
 #include "terrain_generator.hpp"
@@ -17,7 +17,7 @@ TerrainGenerator::TerrainGenerator(int row, int col) :
     this->map = new char*[row];
     for (int i = 0; i < row; i++) {
         this->map[i] = new char[col];
-        std::fill(this->map[i], this->map[i] + col, ' ');
+        std::fill(this->map[i], this->map[i] + col, '0');
     }
 }
 
@@ -30,17 +30,54 @@ TerrainGenerator::~TerrainGenerator() {
 }
 
 void TerrainGenerator::makeMap() {
-    //just a drafting ideas
     std::random_device rd;
     std::mt19937 mt(rd());
-    std::uniform_int_distribution<int> dist(0,2);
-    char terrain[3] = {'^', '~', '.'};
+    std::uniform_int_distribution<int> dist(0,10);
+    for (int r = 0; r < this->row; r++) {
+        char terrain[11] = {' ', '.', '^', '0', 
+                            '0', '0', '0', '0', 
+                            '0', '0', '0',};
+        for (int c = 0; c < this->col; c++) {
+            if (r > 0) {
+                terrain[4] = this->map[r - 1][c]; 
+                if (c > 0) {
+                    terrain[3] = this->map[r - 1][c - 1];
+                }
+                if (c + 1 < this->col) {
+                    terrain[5] = this->map[r - 1][c + 1];
+                }
+            }
+            if (c > 0) {
+                terrain[6] = this->map[r][c - 1];
+            }
+            if (c + 1 < this->col) {
+                terrain[7] = this->map[r][c + 1];
+            }
+            if (r + 1 < this->row) {
+                terrain[9] = this->map[r + 1][c]; 
+                if (c > 0) {
+                    terrain[8] = this->map[r + 1][c - 1];
+                }
+                if (c + 1 < this->col) {
+                    terrain[10] = this->map[r + 1][c + 1];
+                }
+            }
+            char terrain_symbol = '0';
+            while (terrain_symbol == '0') {
+                terrain_symbol = terrain[dist(mt)];
+            }
+            this->map[r][c] = terrain_symbol;
+        }
+    }
+}
+
+void TerrainGenerator::printMap() {
     for (int r = 0; r < this->row; r++) {
         for (int c = 0; c < this->col; c++) {
-            std::cout << terrain[dist(mt)];
-        }
+            std::cout << this->map[r][c];
+        }        
         std::cout << std::endl;
-    }
+    } 
 }
 
 char **TerrainGenerator::getMap() {
